4f3823b6fbc17fa54d300d9f71e0cbaee1959efd
FraSma <francemazzi@gmail.com>
2016-06-21 15:40:00 +0200

    alleggerimento Codice classu - class app

--- script.js
+++ script.js
@@ -14,31 +14,44 @@ const inputElevation = document.querySelector('.form__input--elevation');
 let map, mapEvent;
 
 class App {
+  //private
+  #map;
+  #mapEvent;
+
   constructor() {
     this._getPosition();
+    //
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    //Inseriamo la selezione running|cycling -> inpuType
+    //Andiamo a vedere come sono nascosti i form
+    inputType.addEventListener('change', this._toogleElevationField);
   }
   _getPosition() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        this._loadMap,
-        function gestisciErrore(error) {
-          switch (error.code) {
-            case error.PERMISSION_DENIED:
-              console.log("Permesso negato dall'utente");
-              break;
-            case error.POSITION_UNAVAILABLE:
-              console.log('Impossibile determinare la posizione corrente');
-              break;
-            case error.TIMEOUT:
-              console.log(
-                'Il rilevamento della posizione impiega troppo tempo'
-              );
-              break;
-            case error.UNKNOWN_ERROR:
-              console.log('Si è verificato un errore sconosciuto');
-              break;
-          }
+        this._loadMap.bind(this),
+        function () {
+          alert('Errore connessione del GPS');
         }
+        // function gestisciErrore(error) {
+        //   switch (error.code) {
+        //     case error.PERMISSION_DENIED:
+        //       console.log("Permesso negato dall'utente");
+        //       break;
+        //     case error.POSITION_UNAVAILABLE:
+        //       console.log('Impossibile determinare la posizione corrente');
+        //       break;
+        //     case error.TIMEOUT:
+        //       console.log(
+        //         'Il rilevamento della posizione impiega troppo tempo'
+        //       );
+        //       break;
+        //     case error.UNKNOWN_ERROR:
+        //       console.log('Si è verificato un errore sconosciuto');
+        //       break;
+        //   }
+        // }
       );
     }
   }
@@ -48,59 +61,53 @@ class App {
     console.log(`https://www.google.it/maps/@${latitudine}.${longitudine},14z`);
     const coords = [latitudine, longitudine];
 
-    map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
     //Hanfling clicks on map --> abbiamo la possobiilità di accedere all'evneto map
-    map.on('click', function (mapE) {
-      mapEvent = mapE;
-      form.classList.remove('hidden');
-      inputDistance.focus();
-    });
+    this.#map.on('click', this._shoForm.bind(this));
   }
-  _shoForm() {}
-  _toogleElevationField() {}
-  newWorkout() {}
+  _shoForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  _toogleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+    //Per evitare auto-caricamento
+    e.preventDefault();
+
+    //Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    //display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Allenamento 🐎!')
+      .openPopup();
+    // il primo problema è che dobbiamo utilizzare due variabili che non esistono in questo scope
+  }
 }
 
 const app = new App();
-
-form.addEventListener('submit', function (e) {
-  //Per evitare auto-caricamento
-  e.preventDefault();
-
-  //Clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-
-  //display marker
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('Allenamento 🐎!')
-    .openPopup();
-  // il primo problema è che dobbiamo utilizzare due variabili che non esistono in questo scope
-});
-
-//Inseriamo la selezione running|cycling -> inpuType
-//Andiamo a vedere come sono nascosti i form
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
